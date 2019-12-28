bb96410f28b1a10d0d61380232302062419b00ca
Florian JUDITH <florian.judith.b@gmail.com>
2019-12-28 20:25:54 +0100

    Updated script

--- tools/rasterize.sh
+++ tools/rasterize.sh
@@ -1,19 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
 SIZES=(128 256)
 
 docker build -t svgconvertor:latest -f tools/Dockerfile .
 
-total=$(ls svg/*/*/* | wc -l)
+total=$(find svg/ -name *.svg | wc -l)
 counter=1
 
 for size in "${SIZES[@]}"; do 
-    for svg in $(ls svg/*/*/* | cut -b 5-); do
+    for svg in $(find svg/ -name *.svg | cut -b 5-); do
         dir=$(echo $svg | cut -d / -f-2)
         mkdir -p png/$dir
         output=png/${svg%%.*}-$size.png
         echo "[$counter/$total] Generating $output"
-        docker run -v $(pwd)/svg:/convertor svgconvertor:latest  $svg -h $size -w $size > $output
+        docker run -v $(pwd)/svg:/convertor svgconvertor:latest  $svg -w $size > $output
         counter=$[$counter +1]
     done
 done
